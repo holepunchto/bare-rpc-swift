ac40f9e98f7e11ed6962e756f88fca0a7402e9f6
Balakrishna Avulapati <ba@bavulapati.com>
2026-03-18 22:32:14 +0530

    remove redundant tests
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- Tests/BareRPCTests/RPCErrorTests.swift
+++ Tests/BareRPCTests/RPCErrorTests.swift
@@ -1,17 +1,8 @@
-// Tests/BareRPCTests/RPCErrorTests.swift
 import Testing
 
 @testable import BareRPC
 
 @Suite struct RPCErrorTests {
-  @Test func remoteErrorPreservesAllFields() {
-    let error = RPCRemoteError(message: "not found", code: "ENOENT", errno: -2)
-    #expect(error.message == "not found")
-    #expect(error.code == "ENOENT")
-    #expect(error.errno == -2)
-    #expect(error is Error)
-  }
-
   @Test func remoteErrorDefaultErrno() {
     let error = RPCRemoteError(message: "fail", code: "ERR")
     #expect(error.errno == 0)

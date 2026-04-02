52a7e81dd0189dda7ffd5debe144d67332fabc8b
Balakrishna Avulapati <ba@bavulapati.com>
2026-04-02 19:35:55 +0530

    use _ prefix for private members
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- Sources/BareRPC/IncomingStream.swift
+++ Sources/BareRPC/IncomingStream.swift
@@ -4,35 +4,35 @@ public class IncomingStream {
   public let requestId: UInt
   public let mask: UInt
   public let stream: AsyncThrowingStream<Data, Error>
-  private let continuation: AsyncThrowingStream<Data, Error>.Continuation
-  private var finished = false
+  private let _continuation: AsyncThrowingStream<Data, Error>.Continuation
+  private var _finished = false
 
   public init(requestId: UInt, mask: UInt) {
     self.requestId = requestId
     self.mask = mask
     var cont: AsyncThrowingStream<Data, Error>.Continuation!
     self.stream = AsyncThrowingStream<Data, Error> { cont = $0 }
-    self.continuation = cont
+    self._continuation = cont
   }
 
   func push(_ data: Data) {
-    guard !finished else { return }
-    continuation.yield(data)
+    guard !_finished else { return }
+    _continuation.yield(data)
   }
 
   func end() {
-    guard !finished else { return }
-    finished = true
-    continuation.finish()
+    guard !_finished else { return }
+    _finished = true
+    _continuation.finish()
   }
 
   public func destroy(error: RPCRemoteError? = nil) {
-    guard !finished else { return }
-    finished = true
+    guard !_finished else { return }
+    _finished = true
     if let error {
-      continuation.finish(throwing: error)
+      _continuation.finish(throwing: error)
     } else {
-      continuation.finish()
+      _continuation.finish()
     }
   }
 }

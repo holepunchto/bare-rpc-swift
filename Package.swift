d06ebf165968d6493c793fae12a9a6d0b95b1244
Balakrishna Avulapati <ba@bavulapati.com>
2026-07-15 21:02:53 +0530

    Pin compact-encoding-swift dependency to from: 1.0.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .library(name: "BareRPC", targets: ["BareRPC"])
   ],
   dependencies: [
-    .package(url: "https://github.com/holepunchto/compact-encoding-swift", branch: "main")
+    .package(url: "https://github.com/holepunchto/compact-encoding-swift", from: "1.0.0")
   ],
   targets: [
     .target(

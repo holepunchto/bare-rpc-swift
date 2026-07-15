69f6415cfaf68e607131cd9de6b7899442497876
Balakrishna Avulapati <ba@bavulapati.com>
2026-07-15 21:04:47 +0530

    Make StreamFlag internal to match the JS reference

--- Sources/BareRPC/StreamConstants.swift
+++ Sources/BareRPC/StreamConstants.swift
@@ -1,12 +1,12 @@
-public struct StreamFlag {
-  public static let open: UInt = 0x1
-  public static let close: UInt = 0x2
-  public static let pause: UInt = 0x4
-  public static let resume: UInt = 0x8
-  public static let data: UInt = 0x10
-  public static let end: UInt = 0x20
-  public static let destroy: UInt = 0x40
-  public static let error: UInt = 0x80
-  public static let request: UInt = 0x100
-  public static let response: UInt = 0x200
+struct StreamFlag {
+  static let open: UInt = 0x1
+  static let close: UInt = 0x2
+  static let pause: UInt = 0x4
+  static let resume: UInt = 0x8
+  static let data: UInt = 0x10
+  static let end: UInt = 0x20
+  static let destroy: UInt = 0x40
+  static let error: UInt = 0x80
+  static let request: UInt = 0x100
+  static let response: UInt = 0x200
 }
